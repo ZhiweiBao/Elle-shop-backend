3d38b297d3f7cb32bfbb8a91e2e8c171169066d3
Zhiwei Bao <zhiv.bao@gmail.com>
2021-12-29 20:31:14 +0800

    Checkout Form - Drop-Down Lists

--- src/main/java/edu/neu/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/edu/neu/ecommerce/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package edu.neu.ecommerce.config;
 
+import edu.neu.ecommerce.entity.Country;
 import edu.neu.ecommerce.entity.Product;
 import edu.neu.ecommerce.entity.ProductCategory;
+import edu.neu.ecommerce.entity.State;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -17,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-  private EntityManager entityManager;
+  private final EntityManager entityManager;
 
   @Autowired
   public MyDataRestConfig(EntityManager entityManager) {
@@ -29,17 +31,22 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
       CorsRegistry cors) {
     HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
 
-    config.getExposureConfiguration().forDomainType(Product.class)
-        .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
-        .withCollectionExposure(
-            (metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-    config.getExposureConfiguration().forDomainType(ProductCategory.class)
-        .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
-        .withCollectionExposure(
-            (metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    disableHttpMethods(Product.class, config, theUnsupportedActions);
+    disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+    disableHttpMethods(Country.class, config, theUnsupportedActions);
+    disableHttpMethods(State.class, config, theUnsupportedActions);
 
     exposeIds(config);
+  }
+
+  private void disableHttpMethods(Class theClass,
+      RepositoryRestConfiguration config,
+      HttpMethod[] theUnsupportedActions) {
+    config.getExposureConfiguration()
+        .forDomainType(theClass)
+        .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+        .withCollectionExposure(
+            (metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
   }
 
   private void exposeIds(RepositoryRestConfiguration config) {
