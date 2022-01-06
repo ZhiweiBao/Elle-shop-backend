89082ae87bb54c54ef005e07cc4184a15d643d86
Zhiwei Bao <zhiv.bao@gmail.com>
2022-01-06 19:11:32 +0800

    Credit Card Payment Processing with Stripe

--- src/main/java/edu/neu/ecommerce/controller/CheckoutController.java
+++ src/main/java/edu/neu/ecommerce/controller/CheckoutController.java
@@ -1,8 +1,13 @@
 package edu.neu.ecommerce.controller;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import edu.neu.ecommerce.dto.PaymentInfo;
 import edu.neu.ecommerce.dto.Purchase;
 import edu.neu.ecommerce.dto.PurchaseResponse;
 import edu.neu.ecommerce.service.CheckoutService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +26,13 @@ public class CheckoutController {
   @PostMapping("/purchase")
   public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
     return checkoutService.placeOrder(purchase);
+  }
+
+  @PostMapping("/payment-intent")
+  public ResponseEntity createPaymentIntent(@RequestBody PaymentInfo paymentInfo)
+      throws StripeException {
+    PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+    String paymentStr = paymentIntent.toJson();
+    return new ResponseEntity<>(paymentStr, HttpStatus.OK);
   }
 }
---
+++ src/main/java/edu/neu/ecommerce/dto/PaymentInfo.java
@@ -0,0 +1,11 @@
+package edu.neu.ecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfo {
+
+  private int amount;
+  private String currency;
+  private String receiptEmail;
+}
